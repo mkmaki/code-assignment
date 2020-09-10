9e19d40e1303435b9a836a24958f594f5c9836e1
Mika Maki <mika.maki@helsinki.fi>
2020-09-10 11:21:04 +0300

    refactoring

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const handlers = require ('./lib/handlers')
 
 app.use(cors())
 app.use(express.static('public'))
-
-app.get('/packages/:name', handlers.getPackage)
-app.get('/packages', handlers.getAllPackages)
-
+app.get('/packages/:name', handlers.get)
+app.get('/packages', handlers.getAll)
 app.use((req, res) => res.sendStatus(404))
 app.listen(8080)
 
